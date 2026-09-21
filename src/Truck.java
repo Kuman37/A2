f5a2bf5902396460d17677310deca80a00bf4b9b
Kuman <kuandykov5122@gmail.com>
2026-09-21 11:37:08 +0500

    First steps

---
+++ src/Truck.java
@@ -0,0 +1,6 @@
+public class Truck implements Transport{
+    @Override
+    public void deliver(String cargo, String destination) {
+        System.out.println("Truck delivers " + cargo + " to " + destination + " warehouse by road.");
+    }
+}
